bcc651878534de68f9221ba4f68f6233420f59e0
Manh Dao <manhdao1006@gmail.com>
2025-02-12 15:50:15 +0700

    fix-create-FE

--- liveauctions-frontend/src/App.vue
+++ liveauctions-frontend/src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default defineComponent({
+        name: 'App',
+        components: {
+            HelloWorld
+        }
+    })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- liveauctions-frontend/vue.config.js
+++ liveauctions-frontend/vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    port: 3000
-  }
-})
+module.exports = {
+    devServer: {
+        port: 3000
+    }
+}
