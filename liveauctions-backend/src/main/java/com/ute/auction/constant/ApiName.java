df6f7970cde7409347ffcd848b4c2b5620a5cdca
Manh Dao <manhdao1006@gmail.com>
2025-02-12 09:42:45 +0700

    config-format

--- liveauctions-backend/src/main/java/com/ute/auction/constant/ApiName.java
+++ liveauctions-backend/src/main/java/com/ute/auction/constant/ApiName.java
@@ -1,8 +1,6 @@
 package com.ute.auction.constant;
 
 public class ApiName {
-    public static final String CRUD_ADMIN = "ApiOfCrudAdmin";
-    public static final String SEARCH_ADMIN = "ApiOfSearchAdmin";
-    public static final String CRUD_WEB = "ApiOfCRUDWeb";
-    public static final String SEARCH_WEB = "ApiOfSearchWeb";
+    public static final String API = "ApiOfAdmin";
+    public static final String WEB = "ApiOfWeb";
 }
